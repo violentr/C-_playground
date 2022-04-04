d6a15bf060c340e7f2102552da235ffec38951a6
Deniss Bohanovs <den.b@itservicesgroup.co.uk>
2022-04-04 19:37:00 +0100

    Add Display files in a sructural way /folder/file

--- Linux/myls.c
+++ Linux/myls.c
@@ -4,22 +4,44 @@
 #include <stdlib.h>
 #include <errno.h>
 
-int main(int argc, char * argv[]){
+/*
+ * gcc myls.c -o myls
+ * Usage: ./myls /your/path/here
+ *
+ */
 
+void listdir(const char * pathName){
   DIR *dp;
+  char PATH[258]={0};
 
   struct dirent *dirp;
 
-  if(argc != 2) {
-    printf("%s\n", "Please provide the directory" );
-  }
-  if ((dp = opendir(argv[1])) == NULL){
+  if ((dp = opendir(pathName)) == NULL){
     printf("Cannot open directory error=%s\n", strerror(errno));
     exit(1);
   }
   while ((dirp = readdir(dp)) != NULL){
-    printf("%s\n", dirp->d_name);
+    if (dirp->d_type == DT_DIR){
+      if(strcmp(dirp->d_name, ".") == 0 || strcmp(dirp->d_name, "..") == 0){
+        continue;
+      }
+      //printf("%s/%s\n", pathName, dirp->d_name);
+      snprintf(PATH, sizeof(PATH), "%s/%s", pathName, dirp->d_name);
+      //printf("PATH %s", PATH);
+      listdir(PATH);
+    }else{
+      printf("%s/%s\n",pathName, dirp->d_name);
+    }
   }
   closedir(dp);
+}
+
+int main(int argc, char * argv[]){
+
+
+  if(argc != 2) {
+    printf("%s\n", "Please provide the directory" );
+  }
+  listdir(argv[1]);
   exit(0);
 }
