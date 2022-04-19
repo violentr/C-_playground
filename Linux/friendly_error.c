d8ff14b5a9094b12e5661347da50c70c61f5b0dd
Deniss Bohanovs <den.b@itservicesgroup.co.uk>
2022-04-19 20:18:14 +0100

    Add handle errors with strerror, friendly message

---
+++ Linux/friendly_error.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <string.h>
+#include <errno.h>
+
+void readFile(char * filename){
+  int fd = -1;
+  char buffer[4096] ={0};
+  int ret = 0;
+
+  fd = open("./test1.txt", O_RDONLY);
+
+  if (fd == -1){
+    printf("Error in opening file error_num=%d, %s \n", errno, strerror(errno));
+    exit(EXIT_FAILURE);
+  }
+  while (ret = read(fd, buffer, sizeof(buffer)) > 0) {
+    printf("%s", buffer);
+    memset(buffer, 0, sizeof(buffer));
+  }
+  close(fd);
+  exit(EXIT_SUCCESS);
+}
+
+int main(int argc, char **argv){
+  if(argc != 2){
+    printf("[-] Error: Please provide a filename\n");
+    exit(1);
+  }
+  char * filename = argv[1];
+
+  readFile(filename);
+  return 0;
+}
