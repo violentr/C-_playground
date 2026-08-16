67eeed6ccd463e49cfaa5de4291f1714fb5047e4
Deniss B <den.b@itservicesgroup.co.uk>
2026-08-16 15:37:58 +0100

    Add range-for loop example

--- B_TourC++/ch-1.7.pointers.cpp
+++ B_TourC++/ch-1.7.pointers.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 using namespace std;
 
+void rangeFor() {
+    int arr[] = {1,2,3,4,5,6};
+    for (auto x:arr){
+        cout << x << endl;
+    }
+}
 
 int main(){
   int arr[] = {1,2,3,4,5,6};
@@ -9,4 +15,5 @@ int main(){
   for (int* i=&arr[0]; i< arr + len;i++){
     cout << *i << endl;
   }
+  rangeFor();
 }
