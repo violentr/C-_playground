5e10081b88fdb46a992f0d93fe7625654fd24f4d
Deniss B <den.b@itservicesgroup.co.uk>
2026-09-20 15:19:27 +0100

    Add convert Lowercase and Uppercase chars to ascii code

--- Practice/drills/str_to_ascii.cpp
+++ Practice/drills/str_to_ascii.cpp
@@ -19,7 +19,7 @@ static void from_ascii_to_str(const std::string &str) {
     for (size_t i=0;i<str.size();i+=2) {
       std::string s_char_code = str.substr(i, 2);
       char char_code = std::stoi(s_char_code);
-      output += char_code;
+      output += char_code + 23;
     }
     std::cout << "ASCII Decoded string: " << output << '\n';
 }
@@ -27,15 +27,15 @@ static void from_ascii_to_str(const std::string &str) {
 static void convert_num_to_ascii() {
     /* DOG -> 687971 */
     std::string normal_str, ascii_str;
-    std::cout << "Your string will be uppercased \n";
+    //std::cout << "Your string will be uppercased \n";
     std::cout << "Enter your string: ";
     std::cin >> normal_str;
-    std::transform(normal_str.begin(), normal_str.end(),
+    /*std::transform(normal_str.begin(), normal_str.end(),
         normal_str.begin(),
         [](unsigned char c) { return ::toupper(c); }
-    );
+    );*/
     for (char c: normal_str) {
-        std::string char_code = std::to_string((int)c);
+        std::string char_code = std::to_string((int)c - 23);
         ascii_str += char_code;
     }
     std::cout << "Ascii str: " << ascii_str << '\n';
